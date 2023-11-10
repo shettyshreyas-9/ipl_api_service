6edfde3f6282724c156ce52e86f1afd2513f3289
shettyshreyas-9 <shreyasshetty925@gmail.com>
2023-11-10 16:16:59 +0530

    Created the app.py file & ipl.py file with function for /api/teams

---
+++ ipl.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+ipl_matches = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRy2DUdUbaKx_Co9F0FSnIlyS-8kp4aKv_I0-qzNeghiZHAI_hw94gKG22XTxNJHMFnFVKsO4xWOdIs/pub?gid=1655759976&single=true&output=csv"
+matches = pd.read_csv(ipl_matches)
+
+
+# print(matches.head())
+
+def teamsAPI():
+    teams= list(set(list(matches['Team1']) + list(matches['Team2'])))
+    team_dict = {'teams': teams}
+    return team_dict
